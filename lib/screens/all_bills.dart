0b3f3add57bdde67d7596ab90cb6ca245c92b1b9
bryanbill <emobrya@gmail.com>
2022-06-21 19:24:25 +0300

    ft:Ui - adds bill card

--- lib/screens/all_bills.dart
+++ lib/screens/all_bills.dart
@@ -11,8 +11,57 @@ class _AllBillsState extends State<AllBills> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(centerTitle: true, title: Text("All Bills"), actions: []),
-      body: Container(padding: EdgeInsets.all(10)),
+      appBar: AppBar(
+          centerTitle: true, title: const Text("All Bills"), actions: []),
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        height: MediaQuery.of(context).size.height,
+        child: ListView(children: [
+          Container(
+            height: 150,
+            width: MediaQuery.of(context).size.width,
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: const Color.fromARGB(255, 243, 233, 237)),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Text(
+                      "Food and Beverage",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                    Chip(
+                        backgroundColor: Color.fromARGB(255, 239, 132, 132),
+                        label: Text("Overdue"))
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Text(
+                      "Food",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                    Text("Kes. 23,000")
+                  ],
+                ),
+                const Divider(
+                  color: Colors.black,
+                  thickness: 2.0,
+                ),
+                const Text("John Doe by 5th July, 2022")
+              ],
+            ),
+          )
+        ]),
+      ),
     );
   }
 }
