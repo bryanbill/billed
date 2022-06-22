4c8d92f3e1c3713a95bfebd9ccc2fb4313f68ce0
bryanbill <emobrya@gmail.com>
2022-06-22 11:18:07 +0300

    ft:Category - Implements category picker

--- lib/screens/widgets/AddBillAlert.dart
+++ lib/screens/widgets/AddBillAlert.dart
@@ -37,66 +37,185 @@ class _AddBillAlertState extends State<AddBillAlert> {
   }
 
   void pickCategory() async {
-    showModalBottomSheet(context: context, builder: (_) => CategoryPicker());
+    showModalBottomSheet(
+        context: context,
+        builder: (_) => Container(
+              padding: const EdgeInsets.all(10),
+              height: MediaQuery.of(context).size.height * 0.3,
+              decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(10),
+                      topLeft: Radius.circular(10))),
+              child: ListView(
+                children: [
+                  const Text(
+                    "Select Category",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                  const Divider(
+                    color: Colors.black,
+                    thickness: 2.0,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _categoryController.text = "Food";
+                      });
+                      Navigator.pop(context);
+                    },
+                    child: Row(
+                      children: const [
+                        Icon(
+                          Icons.dining_rounded,
+                          color: Colors.greenAccent,
+                          size: 32,
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          "Food",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _categoryController.text = "Utilities";
+                      });
+                      Navigator.pop(context);
+                    },
+                    child: Row(
+                      children: const [
+                        Icon(
+                          Icons.document_scanner,
+                          color: Colors.amberAccent,
+                          size: 32,
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          "Utilities",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _categoryController.text = "Transport";
+                      });
+                      Navigator.pop(context);
+                    },
+                    child: Row(
+                      children: const [
+                        Icon(
+                          Icons.fire_truck,
+                          color: Colors.blueAccent,
+                          size: 32,
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          "Transport",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _categoryController.text = "Entertainment";
+                      });
+                    },
+                    child: Row(
+                      children: const [
+                        Icon(
+                          Icons.video_collection,
+                          color: Colors.redAccent,
+                          size: 32,
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          "Entertainment",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ));
   }
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: const Text("Add Bill"),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          TextField(
-            focusNode: _nameFocusNode,
-            controller: _nameController,
-            decoration: const InputDecoration(
-              suffixIcon: Icon(Icons.description),
-              labelText: "Bill Name",
-              border: OutlineInputBorder(),
+      content: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              focusNode: _nameFocusNode,
+              controller: _nameController,
+              decoration: const InputDecoration(
+                suffixIcon: Icon(Icons.description),
+                labelText: "Bill Name",
+                border: OutlineInputBorder(),
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          TextField(
-            focusNode: _amountFocusNode,
-            controller: _amountController,
-            decoration: const InputDecoration(
-              suffixIcon: Icon(Icons.numbers),
-              labelText: "Bill Amount",
-              border: OutlineInputBorder(),
+            const SizedBox(
+              height: 10,
             ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          TextField(
-            onTap: pickTime,
-            focusNode: _dateFocusNode,
-            controller: _dateController,
-            keyboardType: TextInputType.none,
-            decoration: const InputDecoration(
-              suffixIcon: Icon(Icons.calendar_month),
-              labelText: "Due Date",
-              border: OutlineInputBorder(),
+            TextField(
+              focusNode: _amountFocusNode,
+              controller: _amountController,
+              decoration: const InputDecoration(
+                suffixIcon: Icon(Icons.numbers),
+                labelText: "Bill Amount",
+                border: OutlineInputBorder(),
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          TextField(
-            onTap: pickCategory,
-            focusNode: _categoryFocusNode,
-            controller: _categoryController,
-            keyboardType: TextInputType.none,
-            decoration: const InputDecoration(
-              suffixIcon: Icon(Icons.category_outlined),
-              labelText: "Category",
-              border: OutlineInputBorder(),
+            const SizedBox(
+              height: 10,
             ),
-          ),
-        ],
+            TextField(
+              onTap: pickTime,
+              focusNode: _dateFocusNode,
+              controller: _dateController,
+              keyboardType: TextInputType.none,
+              decoration: const InputDecoration(
+                suffixIcon: Icon(Icons.calendar_month),
+                labelText: "Due Date",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextField(
+              onTap: pickCategory,
+              focusNode: _categoryFocusNode,
+              controller: _categoryController,
+              keyboardType: TextInputType.none,
+              decoration: const InputDecoration(
+                suffixIcon: Icon(Icons.category_outlined),
+                labelText: "Category",
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ],
+        ),
       ),
       actions: [
         TextButton(
